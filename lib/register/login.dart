bf1d6bad3264a3798a859e5b15d97946e4c6d660
Hamskira <56066633+Hamskira@users.noreply.github.com>
2021-07-13 13:02:00 +0530

    Login button navigtes to ex.dart

--- lib/register/login.dart
+++ lib/register/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:water_meter_app/ex.dart';
+import 'package:water_meter_app/homescreen/homepage.dart';
 import 'package:water_meter_app/register/signup.dart';
 import 'package:water_meter_app/register/textfields.dart';
 
@@ -10,51 +12,54 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final myController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
 
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 120.0,vertical: 20.0),
-              child: Text("Login",
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold
-              ),),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            FieldText("Username/Email", secure: false,),
-            FieldText("Password",secure: true,),
-            ButtonText(
-              colour: Colors.lightBlueAccent,
-                text: "Login",
-              onPressed: (){},
-
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Don't have an account?"),
-                TextButton(
-                  onPressed: (){
-                    Navigator.pushNamed(context, SignupScreen.id);
-                  },
-                  child: Text("Sign Up now!!",
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 120.0,vertical: 20.0),
+                child: Text("Login",
                   style: TextStyle(
-                    color: Colors.black
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold
                   ),),
-                )
-              ],
-            )
-          ],
-        )
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              FieldText("Username/Email", secure: false,control: myController,ontap: (){},),
+              FieldText("Password",secure: true,control: myController, ontap: (){},),
+              ButtonText(
+                colour: Colors.lightBlueAccent,
+                text: "Login",
+                onPressed: (){
+                  Navigator.pushNamed(context, Example.id);
+                },
+
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have an account?"),
+                  TextButton(
+                    onPressed: (){
+                      Navigator.pushNamed(context, SignupScreen.id);
+                    },
+                    child: Text("Sign Up now!!",
+                      style: TextStyle(
+                          color: Colors.black
+                      ),),
+                  )
+                ],
+              )
+            ],
+          )
       ),
     );
   }
