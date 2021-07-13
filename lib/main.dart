1a979ac507ed5d0d91c70166ac42eb1556552109
Hamskira <56066633+Hamskira@users.noreply.github.com>
2021-07-13 14:02:00 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:water_meter_app/register/welcome_screen.dart';
 import 'package:water_meter_app/register/login.dart';
 import 'package:water_meter_app/register/signup.dart';
+import 'package:water_meter_app/homescreen/homepage.dart';
+import 'ex.dart';
 
 
 void main() {
   runApp(
-      MyApp()
+      MyApp(),
   );
 }
 
@@ -18,9 +20,11 @@ class MyApp extends StatelessWidget {
 
       initialRoute: WelcomeScreen.id,
       routes: {
+        Example.id: (context) => Example(),
         WelcomeScreen.id: (context) => WelcomeScreen(),
         LoginScreen.id: (context) => LoginScreen(),
-        SignupScreen.id : (context) => SignupScreen()
+        SignupScreen.id : (context) => SignupScreen(),
+        HomeScreen.id : (context) => HomeScreen()
 
       },
     );
