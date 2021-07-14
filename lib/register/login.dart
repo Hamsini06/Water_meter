9ded4acd34eca93c41b1b94249b4d2ba877de9b3
Mnmluvit <87131291+Mnmluvit@users.noreply.github.com>
2021-07-14 18:16:23 +0530

    made changes

--- lib/register/login.dart
+++ lib/register/login.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:water_meter_app/ex.dart';
-import 'package:water_meter_app/homescreen/homepage.dart';
 import 'package:water_meter_app/register/signup.dart';
 import 'package:water_meter_app/register/textfields.dart';
 import 'package:water_meter_app/Database_helper.dart';
@@ -23,55 +23,60 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 120.0,vertical: 20.0),
-                child: Text("Login",
-                  style: TextStyle(
-                      fontSize: 40.0,
-                      fontWeight: FontWeight.bold
-                  ),),
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              FieldText("Username/Email", secure: false,control: userNameController),
-              FieldText("Password",secure: true,control: passwordController),
-              ButtonText(
-                colour: Colors.lightBlueAccent,
-                text: "Login",
-                onPressed: () async{
-
-
-                  final id = await loginUser();
-                  if(id != null){
-                    Navigator.pushNamed(context, Example.id);
-                  }
-
-
-                },
-
-              ),
-              Row(
+          body: SingleChildScrollView(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Text("Don't have an account?"),
-                  TextButton(
-                    onPressed: (){
-                      Navigator.pushNamed(context, SignupScreen.id);
-                    },
-                    child: Text("Sign Up now!!",
+
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 120.0,vertical: 20.0),
+                    child: Text("Login",
                       style: TextStyle(
-                          color: Colors.black
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold
                       ),),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  FieldText("Username/Email", secure: false,control: userNameController),
+                  FieldText("Password",secure: true,control: passwordController),
+                  ButtonText(
+                    colour: Colors.lightBlueAccent,
+                    text: "Login",
+                    onPressed: () async{
+
+
+                      final id = await loginUser();
+                      if(id != null){
+                        Navigator.pushNamed(context, Example.id);
+                      }
+
+
+                    },
+
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Don't have an account?"),
+                      TextButton(
+                        onPressed: (){
+                          Navigator.pushNamed(context, SignupScreen.id);
+                        },
+                        child: Text("Sign Up now!!",
+                          style: TextStyle(
+                              color: Colors.black
+                          ),),
+                      )
+                    ],
                   )
                 ],
-              )
-            ],
+              ),
+            ),
           )
       ),
     );
@@ -80,11 +85,11 @@ class _LoginScreenState extends State<LoginScreen> {
   Future loginUser() async {
     List<Map<String, dynamic>> data = await dbHelper.checkLogin(userNameController.text, passwordController.text);
     if(data.length > 0){
-      Map<String, dynamic> login_rows = {
+      Map<String, dynamic> loginRows = {
         DatabaseHelper.userName: userNameController.text,
         DatabaseHelper.password: passwordController.text
       };
-      final id = await dbHelper.insert_data(DatabaseHelper.table_login,login_rows);
+      final id = await dbHelper.insert_data(DatabaseHelper.table_login,loginRows);
       print("id inserted in login table is $id");
       return id;
     }
