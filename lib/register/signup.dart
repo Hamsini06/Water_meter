32d187e37b2045db111c6247244a2d2995a36e46
Hamskira <56066633+Hamskira@users.noreply.github.com>
2021-07-15 14:33:38 +0530

    added input validation conditionals using form

--- lib/register/signup.dart
+++ lib/register/signup.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:water_meter_app/register/login.dart';
 import 'package:water_meter_app/register/textfields.dart';
 import 'package:water_meter_app/Database_helper.dart';
 
+
 class SignupScreen extends StatefulWidget {
+
   static const String id = "signup_screen";
 
 
@@ -14,7 +17,12 @@ class SignupScreen extends StatefulWidget {
   _SignupScreenState createState() => _SignupScreenState();
 }
 
-class _SignupScreenState extends State<SignupScreen> {
+class _SignupScreenState extends State<SignupScreen>  {
+bool isValidated = false;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+
   final dbHelper = DatabaseHelper.instance;
 
   final firstNameController = TextEditingController();
@@ -22,6 +30,7 @@ class _SignupScreenState extends State<SignupScreen> {
   final userNameController = TextEditingController();
   final numberController = TextEditingController();
   final passwordController = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -56,40 +65,92 @@ class _SignupScreenState extends State<SignupScreen> {
                     )
                   ],
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
+                Form(
+                  key: _formKey,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
 
-                      FieldText("First Name", secure: false, control: firstNameController),
-                      FieldText("Last Name", secure: false,control: lastNameController ),
-                      FieldText("Username/Email", secure: false,control: userNameController),
-                      FieldText("Mobile Number", secure: false, control: numberController ),
-                      FieldText("Password", secure: true, control: passwordController),
-                      ButtonText(
-                          colour: Colors.lightBlueAccent,
-                          text: "Sign Up",
-                          onPressed: (){
-                            final id = _insert();
-                            if(id != null) {
-                              Navigator.pushNamed(context, LoginScreen.id);
-                            }
-
+                        FieldText("First Name", secure: false, control: firstNameController, validator: (value){
+                          if(value!.isNotEmpty && value.contains(RegExp(r'[0-9]'))){
+                            return "This field takes in only characters";
                           }
-                      ),
-                      // ButtonText(
-                      //     colour: Colors.lightBlueAccent,
-                      //     text: "blah",
-                      //     onPressed: (){
-                      //       _query();
-                      //
-                      //     }
-                      // ),
-                    ],
+                          else if(value.isEmpty)
+                            return"this field is required";
+                        },),
+                        FieldText("Last Name", secure: false,control: lastNameController,validator: (value){
+                          if(value!.isNotEmpty && value.contains(RegExp(r'[0-9]'))){
+                            return "This field takes in only characters";
+                          }
+                          else if(value.isEmpty)
+                            return"this field is required";
+                        },),
+                        FieldText("Username/Email", secure: false,control: userNameController,validator: (value){
+                          if(value!.isNotEmpty && value.contains(RegExp(r'[0-9]'))){
+                            return "This field takes in only characters";
+                          }
+                          else if(value.isEmpty)
+                            return"this field is required";
+                        }, ),
+                        FieldText("Mobile Number", secure: false, control: numberController, validator: (value){
+                          if(value!.isNotEmpty && !value.contains(RegExp(r'[0-9]') )){
+                            return "This field takes in only numbers";
+                          }
+                          else if(value.isEmpty)
+                            return"this field is required";
+                        },
+                        type: TextInputType.number),
+                        FieldText("Password", secure: true, control: passwordController, validator: (value){
+                          if(value!.isEmpty){
+                            return"this field is required";
+                          }
+                          else if(value.length < 6){
+                            return "this field requires atleast 6 characters";
+                          }
+                        },
+                        ),
+                        ButtonText(
+                            colour: Colors.lightBlueAccent,
+                            text: "Sign Up",
+                            onPressed: () async {
+                              if (_formKey.currentState!.validate()) {
+                                isValidated = true;
+                              }
+                              if (isValidated) {
+                                isValidated = false;
+                                final id = await _insert();
+                                if (id != null) {
+                                  Navigator.pushNamed(context, LoginScreen.id);
+                                }
+                                firstNameController.clear();
+                                lastNameController.clear();
+                                userNameController.clear();
+                                numberController.clear();
+                                passwordController.clear();
+                              }
+                            }
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Already have an account?"),
+                            TextButton(
+                              onPressed: (){
+                                Navigator.pushNamed(context, LoginScreen.id);
+                              },
+                              child: Text("Login now!!",
+                                style: TextStyle(
+                                    color: Colors.black
+                                ),),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-
               ],
             ),
           ),
@@ -114,8 +175,9 @@ class _SignupScreenState extends State<SignupScreen> {
   }
 
   void _query() async {
-    final allRows = await dbHelper.queryAllRows();
+    final allRows = await dbHelper.queryAllRows(DatabaseHelper.table);
     print('query all rows: $allRows');
 
   }
+
 }
