dc7f89e5a1421494cc6c625e34a4eec1f8765f3b
Mnmluvit <87131291+Mnmluvit@users.noreply.github.com>
2021-07-19 14:23:00 +0530

    made changes

--- lib/homescreen/Graph.dart
+++ lib/homescreen/Graph.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
-
+import 'package:water_meter_app/Refactored_widgets/graphcard.dart';
 class GraphPage extends StatefulWidget {
 
   @override
   _GraphPageState createState() => _GraphPageState();
 }
-
 class _GraphPageState extends State<GraphPage> {
+
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text("Graph"),
+
+        body:  Column(
+           crossAxisAlignment: CrossAxisAlignment.stretch,
+          children:[
+            Expanded(child: GraphCard()),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Card(
+                  color: Colors.blue,
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Text("hello",
+                    textAlign: TextAlign.center,),
+                  ),
+
+                ),
+              ),
+            )
+
+
+          ]
         ),
       ),
     );
