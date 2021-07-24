6ba8d70a41054bd4925764434661b2a9f1ad2782
Hamskira <56066633+Hamskira@users.noreply.github.com>
2021-07-24 21:06:06 +0530

    Update textfields.dart

--- lib/register/textfields.dart
+++ lib/register/textfields.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
 
 class FieldText extends StatelessWidget {
   final String text;
@@ -66,19 +64,81 @@ class ButtonText extends StatelessWidget {
     );
   }
 }
+class ProfileText extends StatelessWidget {
+
+  bool value;
+  String text;
+  void Function(String)? onChanged;
+
+  ProfileText(this.value, {this.text = "something", this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 15.0, bottom: 5.0),
+      child: TextFormField(
+        onChanged: onChanged,
+        initialValue: text,
+        enabled: value,
+        decoration: InputDecoration(
+            border: UnderlineInputBorder(
+
+            ),
+            contentPadding: EdgeInsets.symmetric(horizontal: 10.0)
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileButton extends StatelessWidget {
+
+  final void Function()? onpressed;
+  final String text;
+  final bool val;
+  final EdgeInsetsGeometry padding;
+  ProfileButton(this.onpressed, this.text, this.val, this.padding);
+
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+      visible: val,
+      child: Padding(
+        padding: padding,
+        child: Material(
+          color:Colors.blueAccent.shade200,
+          borderRadius: BorderRadius.circular(15.0),
+          child: MaterialButton(
+            onPressed: onpressed,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15.0),
+              child: Text(
+                text,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),),
+            ),),
+        ),
+      ),
+    );
+  }
+}
 class ProfileCard extends StatelessWidget {
-  const ProfileCard({required this.value,required this.title,required this.icon});
+  ProfileCard({required this.value,required this.title,required this.icon,required this.cardText, this.onChanged});
 
   final bool value;
   final String title;
   final IconData icon;
+  final String cardText;
+  void Function(String)? onChanged;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding:EdgeInsets.all(8.0),
       child: ListTile(
-        subtitle: ProfileText(value),
+        subtitle: ProfileText(value,text: cardText,onChanged: onChanged,),
 
 
         title:Text(
@@ -96,3 +156,5 @@ class ProfileCard extends StatelessWidget {
     );
   }
 }
+
+
