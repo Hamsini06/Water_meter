789cbf81f149b407844f79269f056b95362b4424
Hamskira <56066633+Hamskira@users.noreply.github.com>
2021-07-24 19:45:59 +0530

    rendering the data on screen and updating

--- lib/homescreen/Profile.dart
+++ lib/homescreen/Profile.dart
@@ -1,8 +1,15 @@
 
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart';
 import 'package:meter_app/register/textfields.dart';
 import 'package:meter_app/networking.dart';
+import 'package:provider/provider.dart';
+
 class ProfilePage extends StatefulWidget {
 
   @override
@@ -11,6 +18,8 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   bool value = false;
+  bool updated = false;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -19,44 +28,39 @@ class _ProfilePageState extends State<ProfilePage> {
           body: ListView(
             children: [
               Container(
+                padding: EdgeInsets.only(top: 30, left: 30, bottom: 30),
 
-                child: Column(
+                child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top:30,left:30),
-                      child: Visibility(
-                        visible: value,
-                        child: GestureDetector(
-                          onTap: (){
-                            setState(() {
-                              value = !value;
-                            });
-                          },
-                          child: Icon(
-                            Icons.arrow_back,
-                            size: 30.0,
-                            color: Colors.white,
-                          ),
+                    Visibility(
+                      visible: value,
+                      child: GestureDetector(
+                        onTap: (){
+                          setState(() {
+                            value = !value;
+                          });
+                        },
+                        child: Icon(
+                          Icons.arrow_back,
+                          size: 30.0,
+                          color: Colors.white,
                         ),
                       ),
                     ),
                     SizedBox(
-                      height: 10,
+                      width: 20,
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(top:60,left:30,bottom: 40),
-                      child: Text(
-                        "User profile",
-                        style: TextStyle(
-                            fontSize: 30,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w700
+                    Text(
+                      "User profile",
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700
 
-
-                        ),
 
                       ),
+
                     )
                   ],
                 ),
@@ -74,30 +78,48 @@ class _ProfilePageState extends State<ProfilePage> {
                       child: Column(
                         children: [
                           ProfileCard(
+                              cardText: Provider.of<Data>(context).data["firstName"],
+                              value: false,
+                              title: "First Name",
+                              icon:Icons.person
+                          ),
+                          ProfileCard(
+                            cardText: Provider.of<Data>(context).data["lastName"],
+                              value: false,
+                              title: 'Last Name',
+                              icon: Icons.person
+                          ),
+                          ProfileCard(
+                            cardText: Provider.of<Data>(context).data["mobileNo"],
+                              value: value,
+                              onChanged: (value) async {
 
-                          value: value,
-                          title: "name",
-                          icon:Icons.person
-                          ),
-                          ProfileCard(
-                              value: value,
-                              title: 'email',
-                              icon: Icons.email
-                          ),
-                          ProfileCard(
-                              value: value,
+                                // if(message["message"] != "" && message["statusCode"] == 200){
+                                //   updated = true;
+                                // }
+                                // else if (message["statusCode"] == 401){
+                                //   ScaffoldMessenger.of(context)
+                                //       .showSnackBar(SnackBar(content: Text('Unauthorized access')));
+                                // }
+                                // else{
+                                //   ScaffoldMessenger.of(context)
+                                //       .showSnackBar(SnackBar(content: Text('Error in updating data')));
+                                // }
+                              },
                               title: 'phone',
                               icon: Icons.call
                           ),
                           ProfileCard(
-                              value: value,
-                              title: 'phone',
-                              icon: Icons.call
+                            cardText: Provider.of<Data>(context).data["username"],
+                              value: false,
+                              title: 'Username',
+                              icon: Icons.supervised_user_circle_rounded
                           ),
                           ProfileCard(
-                              value: value,
-                              title: 'phone',
-                              icon: Icons.call
+                            cardText: "",
+                              value: false,
+                              title: 'meters',
+                              icon: Icons.water_outlined
                           ),
 
 
@@ -116,24 +138,30 @@ class _ProfilePageState extends State<ProfilePage> {
                 color: Colors.white,
                 child: Column(
                   children: [
-                    ProfileButton(() {
+                    ProfileButton(() async {
                       setState(() {
                         value = true;
                       });
+
                     }, "Update", !value, EdgeInsets.symmetric(horizontal: 80.0)),
                     SizedBox(height: 10.0),
                     Row(
                       children: [
-                        ProfileButton((){
-//                           setState(() {
-//                             value = false;
+                        ProfileButton(() async {
+                          setState(() {
+                            value = false;
 
-//                           });
-                           Network dataHelper = Network();
-                           dataHelper.getUserData();
+                          });
+                          Map data = await Network().getUserData();
+                          print(data);
+
                         }, "Back", value, EdgeInsets.only(right: 20.0, left: 50.0)),
                         ProfileButton(() async {
-
+                          Map<dynamic,dynamic> message = await Network().postUpdatedData("9885019770");
+                          if(message["statusCode"] == 200){
+                            Map data = await Network().getUserData();
+                            print(data);
+                          }
                         }, "Save", value, EdgeInsets.only(left: 50.0)),
                       ],
                     )
