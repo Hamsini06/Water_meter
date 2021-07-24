46f80378222468392c97f3bfdeac6af5243d8f13
Hamskira <56066633+Hamskira@users.noreply.github.com>
2021-07-24 19:43:19 +0530

    added provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+// @dart=2.9
+
 import 'package:flutter/material.dart';
-import 'package:water_meter_app/register/welcome_screen.dart';
-import 'package:water_meter_app/register/login.dart';
-import 'package:water_meter_app/register/signup.dart';
-import 'package:water_meter_app/homescreen/homepage.dart';
+import 'package:meter_app/register/welcome_screen.dart';
+import 'package:meter_app/register/login.dart';
+import 'package:meter_app/register/signup.dart';
+import 'package:meter_app/homescreen/homepage.dart';
+import 'package:provider/provider.dart';
+import 'networking.dart';
 import 'ex.dart';
 
 
-void main() {
+
+dynamic token;
+void main() async {
   runApp(
       MyApp(),
   );
@@ -16,19 +22,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return ChangeNotifierProvider<Data>(
+      create: (context)=> Data(),
+      child: MaterialApp(
+        initialRoute: WelcomeScreen.id ,
+        routes: {
+          Example.id: (context) => Example(),
+          WelcomeScreen.id: (context) => WelcomeScreen(),
+          LoginScreen.id: (context) => LoginScreen(),
+          SignupScreen.id: (context) => SignupScreen(),
+          HomeScreen.id: (context) => HomeScreen(),
 
-      initialRoute: WelcomeScreen.id,
-      routes: {
-        Example.id: (context) => Example(),
-        WelcomeScreen.id: (context) => WelcomeScreen(),
-        LoginScreen.id: (context) => LoginScreen(),
-        SignupScreen.id : (context) => SignupScreen(),
-        HomeScreen.id : (context) => HomeScreen()
-
-      },
+        },
+      ),
     );
   }
+
+
 }
-
-
